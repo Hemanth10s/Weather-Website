291fbffce1535f98dcafa595ecc10cdcd485bf09
unknown <hemanths634@gmail.com>
2020-05-07 12:49:33 +0530

    Templates update

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,7 @@ const forecast=(latitude,longitude,callback)=>{
         }else if(body.error){
             callback('Unable to find location',undefined)
         }else{
-            const forecastMessage=body.daily.data[0].summary+' It is currently '+body.currently.temperature+' degrees out.There is a '+body.currently.precipProbability+'% chance of rain.The maximum temperature today is '+body.daily.data[0].temperatureHigh+'degrees with a minimum temperatur of'+body.daily.data[0].temperatureLow+'degrees.'
+            const forecastMessage=body.daily.data[0].summary+' It is currently '+body.currently.temperature+' degrees out.There is a '+body.currently.precipProbability+'% chance of rain.The maximum temperature today is '+body.daily.data[0].temperatureHigh+' degrees with a minimum temperature of '+body.daily.data[0].temperatureLow+'degrees.'
             callback(undefined,forecastMessage)
         }
      
